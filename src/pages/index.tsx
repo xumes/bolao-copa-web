ae9294e361773b11150fbc428fbcf315cfcc7dfc
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-11-01 23:24:17 -0300

    feat: create app UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,91 @@
+import Image from 'next/image'
+import appPreviewImg from '../assets/app-copa-preview.png'
+import logo from '../assets/logo.svg'
+import usersAvatarExampleImg from '../assets/users-avatar-example.png'
+import iconCheckImg from '../assets/icon-check.svg'
+
 interface HomeProps {
-  count: number
+  poolCount: number
 }
+
 
 export default function Home(props: HomeProps) {
   return (
-    <>
-      <h1 className="text-cyan-500 text-4xl">Pools</h1>
-      <p>Count: {props.count.toFixed(0)}</p>
-    </>
+    <div className='max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center'>
+      <main>
+        <Image 
+          src={logo}
+          alt="Bolao do Copa logo"
+        />
+
+        <h1 className='mt-14 text-white text-5xl font-bold leading-tight'>
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
+
+        <div className='mt-10 flex items-center gap-2'>
+          <Image src={usersAvatarExampleImg} alt="users avatar example" />
+
+          <strong className='text-gray-100 text-xl'>
+            <span className='text-ignite-500'>+12.564</span> pessoas já estão usando
+          </strong>
+        </div>
+
+        <form className='mt-10 flex gap-2'>
+          <input
+          className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm'
+            type="text"
+            required
+            placeholder="Qual o nome do seu bolão?"
+          />
+          <button
+          className='bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700'
+            type="submit"
+          >
+            Criar meu bolão
+          </button>
+        </form>
+
+        <p className='mt-4 text-sm text-gray-300 leading-relaxed'>
+          Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀
+        </p>
+
+        <div className='mt-10 pt-10 border-t border-gray-600 flex items-stretch justify-between text-gray-100'>
+          <div className='flex items-center gap-6'>
+            <Image src={iconCheckImg} alt="" />
+            <div className='flex flex-col'>
+              <span className='font-bold text-2xl'>+{props.poolCount}</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+
+          <div className='w-px items-center bg-gray-600'/>
+
+          <div className='flex items-center gap-6'>
+            <Image src={iconCheckImg} alt="" />
+            <div className='flex flex-col'>
+              <span className='font-bold text-2xl'>+2.034</span>
+              <span>Palpites enviados</span>
+            </div>
+          </div>
+        </div>
+      </main>
+      
+      <Image 
+        src={appPreviewImg} 
+        alt="Two mobile devices displaying the Bolao Copa mobile app preview"
+        quality={100}
+      />
+    </div>
   )
 }
 
 export const getServerSideProps = async () => {
   const response = await fetch(' http://localhost:3333/pools/count')
   const data = await response.json()
-  console.log(data)
 
   return {
     props: {
-      count: data.count
+      poolCount: data.count
     }
   }
 }
