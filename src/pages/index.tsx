5ddb6083a2952ffc82d4e19aeb48b33e6565a25e
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-11-01 23:51:28 -0300

    feat: add new Bolao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import logo from '../assets/logo.svg'
 import usersAvatarExampleImg from '../assets/users-avatar-example.png'
 import iconCheckImg from '../assets/icon-check.svg'
 import { api } from '../lib/axios'
+import { FormEvent, useState } from 'react'
 
 interface HomeProps {
   poolCount: number
@@ -11,8 +12,32 @@ interface HomeProps {
   userCount: number
 }
 
-
 export default function Home(props: HomeProps) {
+  const [poolTitle, setPoolTitle] = useState("")
+  const [poolCode, setPoolCode] = useState("")
+
+  async function createPool(event: FormEvent) {
+    event.preventDefault()
+    if (poolTitle) {
+      try {
+        const response = await api.post('/pools', {
+          title: poolTitle
+        })
+
+        const {code} = response.data
+
+        setPoolCode(code)
+
+        await navigator.clipboard.writeText(code)
+
+        setPoolTitle("")
+      }
+      catch(error) {
+        console.log("error", error)
+        alert("Falha ao criar o bolão, tente novamente!")
+      }
+    }
+  }
   return (
     <div className='max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center'>
       <main>
@@ -33,9 +58,11 @@ export default function Home(props: HomeProps) {
           </strong>
         </div>
 
-        <form className='mt-10 flex gap-2'>
+        <form onSubmit={createPool} className='mt-10 flex gap-2'>
           <input
-          className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm'
+            onChange={event => setPoolTitle(event.target.value)}
+            value={poolTitle}
+            className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-100'
             type="text"
             required
             placeholder="Qual o nome do seu bolão?"
@@ -48,9 +75,17 @@ export default function Home(props: HomeProps) {
           </button>
         </form>
 
-        <p className='mt-4 text-sm text-gray-300 leading-relaxed'>
-          Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀
-        </p>
+        {
+          poolCode && <p className='mt-4 text-sm text-gray-300 leading-relaxed'>
+            <span className='font-bold text-2xl text-ignite-500'>{poolCode}</span> Este é o seu código, compartilhe com seus amigos. Ele já foi copiado para a sua área de transferência!
+          </p>
+            
+        }
+        {
+          !poolCode && <p className='mt-4 text-sm text-gray-300 leading-relaxed'>
+            Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀
+          </p>
+        }
 
         <div className='mt-10 pt-10 border-t border-gray-600 flex items-stretch justify-between text-gray-100'>
           <div className='flex items-center gap-6'>
